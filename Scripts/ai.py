237bde846546bc4eaa235d2afe9bc16766ecb754
wantfun <sasha.petelinos.live@gmail.com>
2024-12-28 01:50:33 +0300

    28.12

--- Scripts/ai.py
+++ Scripts/ai.py
@@ -9,6 +9,7 @@ def ai(player):
 	print(error)
 
 	if error <= 100 and error >= 26:
-		bot.goto(player.xcor() + random.randrange(10, 30), player.ycor())
-	elif error >= 0 and error <= 25:
 		bot.goto(player.xcor() + 10, player.ycor())
+
+	elif error >= 0 and error <= 1:
+		bot.goto(player.xcor() + random.randrange(10, 30), player.ycor())
