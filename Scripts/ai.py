e6d3b69f0b698d5de0f47180a936912cbc8abf1e
wantfun <sasha.petelinos.live@gmail.com>
2024-12-28 02:09:59 +0300

    28.12

--- Scripts/ai.py
+++ Scripts/ai.py
@@ -8,8 +8,17 @@ def ai(player):
 
 	print(error)
 
-	if error <= 100 and error >= 26:
-		bot.goto(player.xcor() + 10, player.ycor())
+	if error <= 100 and error >= 2:
+		sdvg = random.randrange(0, 101)
+
+		if sdvg >= 16:
+			bot.forward(1)
+		elif sdvg >= 0 and sdvg <= 5:
+			bot.left(180)
+		elif sdvg >= 6 and sdvg <= 10:
+			bot.left(90)
+		elif sdvg >= 11 and sdvg <= 15:
+			bot.right(90)
 
 	elif error >= 0 and error <= 1:
-		bot.goto(player.xcor() + random.randrange(10, 30), player.ycor())
+		bot.goto(player.xcor(), player.ycor())
--- Scripts/with_ai.py
+++ Scripts/with_ai.py
@@ -12,24 +12,17 @@ def dvizh():
         try:  # для того, чтобы не выскакивала ошибка, что нажата другая кнопка
             if keyboard.is_pressed('w') or keyboard.is_pressed('up'):  # идём вперёд при нажатии на "w" или "ц" или на стрелку вверх
                 player.forward(1)
-                ai.ai(player)
-                #движение бота
-                continue #следующая итерация
             elif keyboard.is_pressed('s') or keyboard.is_pressed('down'): # поворачиваемся назад при нажатии на "s" или "ы" или на стрелку вниз
                 player.left(180)
-                #движение бота
-                continue #следующая итерация
             elif keyboard.is_pressed('d') or keyboard.is_pressed('right'):  # поворачиваемся направо при нажатии на "d" или "в" или на стрелку вправо
                 player.right(90)
-                #движение бота
-                continue #следующая итерация
             elif keyboard.is_pressed('a') or keyboard.is_pressed('left'):  # поворачиваемся налево при нажатии на "ф" или "a" или на стрелку влево
                 player.left(90)
-                #движение бота
-                continue #следующая итерация
             elif keyboard.is_pressed('esc'):  # выходим в магазин при нажатии на "esc"
                 #import shop
                 #функция открытия магазина
                 break #конец итерации
+            ai.ai(player) #движение бота
+            continue #следующая итерация
         except:
             continue
