0a1aec5aafa763bbe3ad972a85ccbdd2a08bb67b
wantfun <sasha.petelinos.live@gmail.com>
2024-12-28 01:16:34 +0300

    28.12

--- Scripts/ai.py
+++ Scripts/ai.py
@@ -4,9 +4,9 @@ import random
 bot = turtle.Turtle()
 
 def ai(player):
-	error = random.randrange(0, 1)
+	error = random.randrange(0, 2)
 
 	if error == 0:
 		bot.goto(player.xcor() + 10, player.ycor())
-	if error == 1:
+	elif error == 1:
 		bot.goto(player.xcor() + random.randrange(10, 30), player.ycor())
