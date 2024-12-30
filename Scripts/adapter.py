b4160196d7dd13738a6b7d4eb47eb9ebf1b7fd3b
wantfun <sasha.petelinos.live@gmail.com>
2024-12-30 06:14:57 +0300

    30.12

--- Scripts/adapter.py
+++ Scripts/adapter.py
@@ -2,5 +2,6 @@ import with_ai
 
 def transition():
 	with_ai.dvizh()
+
 def change_shape():
 	with_ai.player.shape('turtle')
--- Scripts/shop.py
+++ Scripts/shop.py
@@ -16,48 +16,48 @@ def skin():
 	c.execute('SELECT bal FROM wallet WHERE name = ?', ('player',)) #проверяем наличие до этого человека в базе
 	bal = c.fetchone()
 
-	if bal == None: #если нет-то создаём его
+	if bal == None: #если нет, то присваиваем ей значение 0
 		bal = 0
 
-	if bal[0] >= 25:
-		c.execute('UPDATE wallet SET bal = bal - 25 WHERE name = ?', ('player',)) #добавляем одну монетку
+	if bal[0] >= 25: #если баланс больше или равен стоимости покупки
+		c.execute('UPDATE wallet SET bal = bal - 25 WHERE name = ?', ('player',)) #вычисляем стоимость
 		conn.commit()
 
-		adapter.change_shape()
-		showinfo(title=f"Успех!", message=f'Из мальчика, я превращаю в черепашку!')
+		adapter.change_shape() #меняем скин
+		showinfo(title=f"Успех!", message=f'Из мальчика, я превращаю в черепашку!') #просто рофл
 
-	if bal[0] < 25:
-		showinfo(title=f"Безуспешно", message=f'Денег не хватает(')
+	if bal[0] < 25: #если баланс меньше, чем стоимость покупи
+		showinfo(title=f"Безуспешно", message=f'Денег не хватает(') #отправляем предупреждение, что не хватает денег на покупку
 
 def get_bal():
 	c.execute('SELECT bal FROM wallet WHERE name = ?', ('player',)) #проверяем наличие до этого человека в базе
 	bal = c.fetchone()
 
-	if bal == None:
+	if bal == None: #если нет, то присваиваем ей значение 0
 		bal = 0
 
-	showinfo(title=f"Ваш баланс", message=f'Баланс: {bal[0]}')
+	showinfo(title=f"Ваш баланс", message=f'Баланс: {bal[0]}') #отправляем баланс
 
 def open_shop():
-	shoping_centre = Tk()
+	shoping_centre = Tk() #создаём окошко магазина
 	shoping_centre.title("Магазин") #даём ему имя
-	shoping_centre.geometry("300x250") #задём масштаб
+	shoping_centre.geometry("300x250") #задаём масштаб
 
-	bt1 = Button(shoping_centre, text = 'Сделать черепашку - 25 монет', width=25, command=skin).pack(anchor=CENTER,expand=True)
+	bt1 = Button(shoping_centre, text = 'Сделать черепашку - 25 монет', width=25, command=skin).pack(anchor=CENTER,expand=True) #создаём кнопку для покупки
 
 def shop():
-	window = Tk()
+	window = Tk() #создаём окошко паузы
 	
 	window.title("Пауза") #даём ему имя
 	window.geometry("300x250") #задём масштаб
 
-	def exit_from_shop():
+	def exit_from_shop(): #выход из магазина при закрытии окна
 		window.destroy()
 		adapter.transition()
 	
-	bt1 = Button(window, text = 'Показать баланс', width=15, command=get_bal).pack(anchor=CENTER,expand=True)
+	bt1 = Button(window, text = 'Показать баланс', width=15, command=get_bal).pack(anchor=CENTER,expand=True) #создаём кнопки выбора действий (показать баланс или войти в магазин)
 	bt2 = Button(window, text = 'Магазин', width=10, command=open_shop).pack(anchor=CENTER,expand=True)
 
-	window.protocol("WM_DELETE_WINDOW", exit_from_shop)
+	window.protocol("WM_DELETE_WINDOW", exit_from_shop) #проверка на закрытие окна через крестик
 
 	window.mainloop()
